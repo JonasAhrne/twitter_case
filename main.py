da25e2504db85aa300795a78ba357b5d72ad39d7
JonasAhrne <jonas_ahrne@hotmail.com>
2022-06-28 08:49:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-
-import requests
-import os
-import json
 import methods
 import urllib.parse
 
@@ -13,21 +9,5 @@ max_results = 10
 
 url = f'https://api.twitter.com/2/tweets/search/recent?query={urllib.parse.quote(full_query)}&tweet.fields={add_column}&max_results={max_results}'
 
-def main(url):
-
-    next_token = 'N/A'
-    while next_token != '' or next_token == 'N/A':
-        
-        if next_token == 'N/A': # First run
-            json_response = methods.connect_to_endpoint(url)
-        else:
-            json_response = methods.connect_to_endpoint(f'{url}&next_token={next_token}')
-
-        #print(json_response["meta"]["next_token"])
-        methods.pass_to_pubsub(json_response)
-        next_token = (json_response["meta"]["next_token"])
-
 if __name__ == "__main__":
-
-    main(url)
-
+    methods.call_api_to_pubsub(url)
