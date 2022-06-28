4fe600efe799d53884add60324e5e475bf770c8e
JonasAhrne <jonas_ahrne@hotmail.com>
2022-06-28 07:34:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
-import os
 
 ### Twitter API variables
-api_key = 'TGnTOXsIQdyG5TBJ4ooMD4CkR'
-api_secret = 'ED2ygUHcSuWu4seLUp5ILRwHbmVFtKLbBLRvOiG1OWspQn26n1'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAABZNeAEAAAAAgpEpPRK5N0d5HeKwgY7lDPbbXcs%3DppswWwlaRVVq3mjBZHYIyg4niqDosfzZxIo2yPeJ2bCdi6iwVX'
+api_key = '[YOUR TWITTER API KEY]'
+api_secret = '[YOUR TWITTER API SECRET]'
+bearer_token = '[YOUR TWITTER BEARER TOKEN]'
 
 #gcloud variables
-project_id = 'ikea-twitter-220'
-pubsub_topic = 'projects/ikea-twitter-220/topics/twitter-feed'
-credentials_path = 'twitter-app/key/ikea-twitter-private-key.json'
+project_id = '[PROJECT ID]'
+pubsub_topic = '[PUBSUB TOPIC]'
+credentials_path = '[PRIVATE KEY PATH]'
 
